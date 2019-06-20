3bd81e8ce3d2b5d687e6eda2d096a53da116ea31
xuxu <xuxu@DESKTOP-SJJT0CA>
2019-06-20 14:15:20 +0800

    ok

---
+++ cloud-zuul/src/main/java/com/pgy/cloud/gateway/CloudGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.pgy.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+
+
+
+@SpringCloudApplication
+public class CloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudGatewayApplication.class, args);
+	}
+
+}
